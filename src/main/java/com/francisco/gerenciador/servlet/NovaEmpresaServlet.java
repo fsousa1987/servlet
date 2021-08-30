fddfe9b55e99584a4b62645244009c2cea4c4665
Francisco Sousa <franciscoeds1987@gmail.com>
2021-08-30 07:24:52 -0300

    Testando o envio de parâmetros

---
+++ src/main/java/com/francisco/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,21 @@
+package com.francisco.gerenciador.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NovaEmpresaServlet", value = "/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("Cadastrando nova empresa");
+        String nomeEmpresa = req.getParameter("nome");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>Empresa "+ nomeEmpresa +" cadastrada com sucesso!</body></html>");
+    }
+}
