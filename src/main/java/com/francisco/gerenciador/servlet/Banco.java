41c41a1a92fd8a7b60130b6a857a5c0affdf325b
Francisco Sousa <franciscoeds1987@gmail.com>
2021-08-30 20:29:58 -0300

    Definindo o modelo

---
+++ src/main/java/com/francisco/gerenciador/servlet/Banco.java
@@ -0,0 +1,17 @@
+package com.francisco.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+    private static List<Empresa> lista = new ArrayList<>();
+
+    public void adiciona(Empresa empresa) {
+        Banco.lista.add(empresa);
+    }
+
+    public List<Empresa> getEmpresas() {
+        return Banco.lista;
+    }
+}
--- src/main/java/com/francisco/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/com/francisco/gerenciador/servlet/NovaEmpresaServlet.java
@@ -14,7 +14,14 @@ public class NovaEmpresaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("Cadastrando nova empresa");
+
         String nomeEmpresa = req.getParameter("nome");
+        Empresa empresa = new Empresa();
+        empresa.setNome(nomeEmpresa);
+
+        Banco banco = new Banco();
+        banco.adiciona(empresa);
+
         PrintWriter out = resp.getWriter();
         out.println("<html><body>Empresa "+ nomeEmpresa +" cadastrada com sucesso!</body></html>");
     }
