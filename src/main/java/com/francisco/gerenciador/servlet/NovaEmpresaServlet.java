e72b598af95f97e1fa6ff211b850b4898459e45e
Francisco Sousa <franciscoeds1987@gmail.com>
2021-08-31 07:50:29 -0300

    Despachando a requisição

--- src/main/java/com/francisco/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/com/francisco/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,18 +1,19 @@
 package com.francisco.gerenciador.servlet;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "NovaEmpresaServlet", value = "/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         System.out.println("Cadastrando nova empresa");
 
         String nomeEmpresa = req.getParameter("nome");
@@ -22,7 +23,9 @@ public class NovaEmpresaServlet extends HttpServlet {
         Banco banco = new Banco();
         banco.adiciona(empresa);
 
-        PrintWriter out = resp.getWriter();
-        out.println("<html><body>Empresa "+ nomeEmpresa +" cadastrada com sucesso!</body></html>");
+        // chamar o JSP
+        RequestDispatcher rd = req.getRequestDispatcher("/novaEmpresaCriada.jsp");
+        req.setAttribute("empresa", empresa.getNome());
+        rd.forward(req, resp);
     }
 }
