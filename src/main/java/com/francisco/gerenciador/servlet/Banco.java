2af6e37eedebde3b3fe78a1a14ab811a61c504cd
Francisco Sousa <franciscoeds1987@gmail.com>
2021-09-02 07:38:03 -0300

    Removendo empresa

--- src/main/java/com/francisco/gerenciador/servlet/Banco.java
+++ src/main/java/com/francisco/gerenciador/servlet/Banco.java
@@ -6,12 +6,15 @@ import java.util.List;
 public class Banco {
 
     private static final List<Empresa> lista = new ArrayList<>();
+    private static Integer chaveSequencial = 1;
 
     static {
         Empresa empresa = new Empresa();
+        empresa.setId(Banco.chaveSequencial++);
         empresa.setNome("Alura");
 
         Empresa empresa2 = new Empresa();
+        empresa2.setId(Banco.chaveSequencial++);
         empresa2.setNome("Caelum");
 
         lista.add(empresa);
@@ -19,10 +22,15 @@ public class Banco {
     }
 
     public void adiciona(Empresa empresa) {
+        empresa.setId(Banco.chaveSequencial++);
         Banco.lista.add(empresa);
     }
 
     public List<Empresa> getEmpresas() {
         return Banco.lista;
+    }
+
+    public void removeEmpresa(Integer id) {
+        lista.removeIf(emp -> emp.getId().equals(id));
     }
 }
