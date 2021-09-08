4e88e74b7643c6c0be2253c31ea8fe08a347f7de
Francisco Sousa <franciscoeds1987@gmail.com>
2021-09-08 08:18:09 -0300

    Adicionado serviço com JSON

---
+++ src/main/java/com/francisco/gerenciador/servlet/EmpresasService.java
@@ -0,0 +1,28 @@
+package com.francisco.gerenciador.servlet;
+
+import com.francisco.gerenciador.modelo.Banco;
+import com.francisco.gerenciador.modelo.Empresa;
+import com.google.gson.Gson;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "EmpresasService", value = "/empresas")
+public class EmpresasService extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        List<Empresa> empresas = new Banco().getEmpresas();
+
+        Gson gson = new Gson();
+        String json = gson.toJson(empresas);
+
+        resp.setContentType("application/json");
+        resp.getWriter().print(json);
+    }
+}
